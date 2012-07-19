bd2806f6bae5c1a13a065be1166a6251ee5dc008
akshayhegde <xan1790@gmail.com>
2012-07-18 19:30:38 -0700

    made some more progress and changed the layout of the gui

--- src/pintosim/GUI.java
+++ src/pintosim/GUI.java
@@ -1,6 +1,7 @@
 package pintosim;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
 
 /**
@@ -9,6 +10,15 @@ import java.awt.event.*;
  * @author PlzSendTheCodes team
  */
 public class GUI implements ActionListener, ItemListener {
+
+    // GUI objects
+    private JFrame frame = new JFrame("PintoSim");
+
+    // Backend objects
+    private CommandParser interpreter;
+    private EnviornmentMap map;
+    private PintoManager pintoManager;
+    private Command potentialGetItemCancelationCommand;
 
     public static void main(String[] args) {
         new GUI();
@@ -23,28 +33,81 @@ public class GUI implements ActionListener, ItemListener {
         this.map = map;
         this.pintoManager = pintoManager;*/
 
+        JMenuBar menu = new JMenuBar();
+        JMenu aboutMenu = new JMenu("About");
         frame.setJMenuBar(menu);
-        menu.add(fileMenu);
-        menu.add(editMenu);
-        menu.add(helpMenu);
         menu.add(aboutMenu);
 
-        //Buttons
-        JButton location = new JButton("Note a Location");
-        location.setBounds(0, 0, 30, 15);
+        // Create a JPanel that flows from left to right for JButtons
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+        // note the location
+        JButton location = new JButton("Note a location");
+        location.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JOptionPane.showMessageDialog(frame,
+                        "Work in progress",
+                        "Location",
+                        JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+
+        // retrieve an item
         JButton retrieve = new JButton("Retrieve an item");
-        retrieve.setLocation(20,0);
-        frame.add(location);
-        //frame.add(retrieve);
+        retrieve.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JOptionPane.showMessageDialog(frame,
+                        "Work in progress",
+                        "Retrieve",
+                        JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
 
-        // Edit menu
-        JMenuItem chooseMap = new JMenuItem("Load Map");
-        editMenu.add(chooseMap);
-        editMenu.addActionListener(this);
+        // get status
+        JButton status = new JButton("Check Status");
+        status.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JOptionPane.showMessageDialog(frame,
+                        "Work in progress",
+                        "Status",
+                        JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
 
-        // Help menu
-        JMenuItem helpDesk = new JMenuItem("Call Help Desk");
-        helpMenu.add(helpDesk);
+        // cancel a command
+        JButton cancel = new JButton("Cancel");
+        cancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JOptionPane.showMessageDialog(frame,
+                        "Work in progress",
+                        "Cancel",
+                        JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+
+        // call help desk
+        JButton help = new JButton("Call help desk");
+        help.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JOptionPane.showMessageDialog(frame,
+                        "Work in progress",
+                        "Help Desk",
+                        JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+
+        // Add all the buttons to the panel
+        panel.add(location);
+        panel.add(retrieve);
+        panel.add(cancel);
+        panel.add(help);
+        frame.add(panel);
 
         // About menu
         JMenuItem aboutTeam = new JMenuItem("About PintoSim");
@@ -53,16 +116,28 @@ public class GUI implements ActionListener, ItemListener {
 
         // General frame stuff
         JFrame.setDefaultLookAndFeelDecorated(true);
-        frame.setSize(500, 500);
+        frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
 
     // Manages actions on a object
     public void actionPerformed(ActionEvent e) {
-        if (e.getActionCommand().equalsIgnoreCase("About PintoSim")) {
+        if (e.getActionCommand().equalsIgnoreCase("Note a location")) {
+
+        }
+        else if (e.getActionCommand().equalsIgnoreCase("Retrieve an item")) {
+
+        }
+        else if (e.getActionCommand().equalsIgnoreCase("Check Status")) {
+
+        }
+        else if (e.getActionCommand().equalsIgnoreCase("Cancel")) {
+
+        }
+        else if (e.getActionCommand().equalsIgnoreCase("About PintoSim")) {
             JOptionPane.showMessageDialog(frame,
-                    "Designed by PlzSendTheCodes team\n",
+                    "Designed by PlzSendTheCodes teams",
                     "About PintoSim",
                     JOptionPane.INFORMATION_MESSAGE);
         }
@@ -72,18 +147,4 @@ public class GUI implements ActionListener, ItemListener {
     public void itemStateChanged(ItemEvent e) {
 
     }
-
-    // GUI objects
-    private JFrame frame = new JFrame("PintoSim");
-    private JMenuBar menu = new JMenuBar();
-    private JMenu fileMenu = new JMenu("File");
-    private JMenu editMenu = new JMenu("Edit");
-    private JMenu helpMenu = new JMenu("Help");
-    private JMenu aboutMenu = new JMenu("About");
-
-    // Backend objects
-    private CommandParser interpreter;
-    private EnviornmentMap map;
-    private PintoManager pintoManager;
-    private Command potentialGetItemCancelationCommand;
 }

0bb1b0dd43e5e132a25e0734093f5acef8ed6cb7
akshayhegde <xan1790@gmail.com>
2012-07-23 19:25:30 -0700

    Broke up the gui file and moved the drawing of the whole map to another class; added GuiEnvironmentMap

---
+++ src/pintosim/GuiEnvironmentMap.java
@@ -0,0 +1,37 @@
+package pintosim;
+import java.awt.geom.*;
+
+/**
+ * Draws the map for the GUI version of PintoSim. Takes
+ * care of items addition/removal and updates the map.
+ */
+public class GuiEnvironmentMap {
+
+    private int x;
+    private int y;
+
+    /**
+     * Constructs a GuiEnvironmentMap object
+     */
+    public GuiEnvironmentMap() {
+        // wip...
+    }
+
+    /**
+     * Draws the map and updates items when necessary
+     */
+    public void draw() {
+
+        // Draw the docking station
+        Rectangle2D.Double dockingStation = new Rectangle2D.Double();
+
+    }
+
+    /**
+     * Adds items to the map
+     * @param itemName the name of the item
+     */
+    public void addItem(String itemName) {
+
+    }
+}
